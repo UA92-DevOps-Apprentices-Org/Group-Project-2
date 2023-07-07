43aa80fb26d000027113bdf7ca01c9901043cd1d
Alex Crease <119538960+Apollo-XIV@users.noreply.github.com>
2023-07-07 16:05:19 +0000

    fixed more hero section issues

--- devnexus/app/page.tsx
+++ devnexus/app/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
                 transition: transform 30ms ease-out;            
         `}</style>
         {/* Hero Section */}
-        <section className="w-screen h-screen bg-slate-100">
-            <Hero className="">
+        <section className="w-screen h-screen bg-slate-100 relative">
+            <Hero className="bottom-24 inset-6">
                 <h1 className="self-center relative font-bold left-10 text-7xl">Meet Your Tech Goals<br/> with <span>DevNexus</span></h1>
             </Hero>
             <div className="flex absolute bottom-6 left-6 right-6 items-center">
--- devnexus/components/Hero/Hero.tsx
+++ devnexus/components/Hero/Hero.tsx
@@ -19,7 +19,9 @@ export default function Hero({ children, className }: Props) {
     const visibility = useMotionValue("visible");
     const willChange = useWillChange();
 
-    const {scrollY} = useScroll();
+    const {scrollY, scrollYProgress} = useScroll();
+
+    // Div Height Transforms
     const chain = useTransform(scrollY, (value) => {
         const out = 600 - 0.61*value;
         if (out < 50) {
@@ -31,14 +33,18 @@ export default function Hero({ children, className }: Props) {
     })
     const height = useSpring(chain, {stiffness: 1000, damping:10, bounce: 0, mass: 0.01, restDelta: 0.5})
 
+    // Opacity Transforms
+    const opacityChain = useTransform(scrollYProgress, [0,0.2], [1,0])
+
+
     return <>
-    <div id="box" className={className} style={{height: "1000px"}}>
+    <div id="box" className={className} style={{height: "1000px", position: "relative"}}>
         <motion.div id="top-half" style={{height, willChange, visibility}} className="fixed height-3/4 min-h-96 right-6 left-6 top-6 overflow-hidden bg-slate-800 rounded-[33px] origin-top"/>
-        <motion.div id="bottom-half" style={{height, willChange, visibility}} className="rounded-[33px] absolute bottom-6 right-6 left-6 height-3/4 bg-slate-800 origin-bottom"/>
+        <motion.div id="bottom-half" style={{height, willChange, visibility}} className="rounded-[33px] right-0 left-0 bottom-0 absolute height-3/4 bg-slate-800 origin-bottom"/>
             
-        <div id="insert" className="fixed top-6 flex place-items-center">
+        <motion.div id="insert" style={{opacity: opacityChain}} className="h-full w-full fixed top-6 flex place-items-center">
             {children}
-        </div>
+        </motion.div>
     </div>
     </>
 }
