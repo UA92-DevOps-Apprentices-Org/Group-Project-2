7c72d0f7c32a556f534a5fb2959dafb1721fae22
Alex Crease <119538960+Apollo-XIV@users.noreply.github.com>
2023-08-30 20:31:55 +0100

    tests

--- infra/tf/example.terraform.tfvars
+++ infra/tf/example.terraform.tfvars
@@ -1,2 +1,9 @@
-aws_access_key=
-aws_secret_key=
+aws_access_key = ""
+aws_secret_key = ""
+cidr = ""
+private_subnets = [""]
+public_subnets = [""]
+availability_zones = [""]
+environment = ""
+name = ""
+container_port = 0
--- infra/tf/variables.tf
+++ infra/tf/variables.tf
@@ -6,3 +6,12 @@ variable "aws_access_key" {
 variable "aws_secret_key" {
     type = string
 }
+
+// settings
+variable "cidr" {}
+variable "private_subnets" {}
+variable "public_subnets" {}
+variable "availability_zones" {}
+variable "environment" {}
+variable "name" {}
+variable "container_port" {}
